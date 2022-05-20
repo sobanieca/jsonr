4a23e293b8d9a2e59a657cc65068358a2e8eadde
Adam Sobaniec <adam.sobaniec@softwareone.com>
2022-05-20 16:24:11 +0200

    tests refactor

--- test/test-api.js
+++ test/test-api.js
@@ -2,24 +2,31 @@ import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 const app = new Application();
 const router = new Router();
 
-router.get("/sample-get", (context) => {
+router.get("/sample", (context) => {
  context.response.body = { id: "sample-get" };
 });
 
-router.post("/sample-post", (context) => {
+router.post("/sample", (context) => {
   context.response.body = { id: "sample-post" };
 });
 
-router.delete("/sample-delete", (context) => {
+router.delete("/sample", (context) => {
   context.response.status = 204;
 });
 
-router.put("/sample-put", (context) => {
+router.put("/sample", (context) => {
   context.response.body = { id: "sample-put", ...context.request.body };
 });
 
-router.get("/sample-exception", (context) => {
+router.get("/exception", (context) => {
   throw new Error("Sample exception");
+});
+
+router.get("/auth-required", (context) => {
+  if (context.request.headers().get("Authorization") == "123")
+    context.response.body = { id: "sample-post" }
+  else
+    context.response.status = 401;
 });
 
 app.use(router.routes());
