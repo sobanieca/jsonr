f24f1d5df0202eb0b2d5b11d270955a69088b7e9
Adam Sobaniec <sobanieca@gmail.com>
2023-05-26 22:26:28 +0200

    test adjustment + vim settings

--- test/e2e/test.js
+++ test/e2e/test.js
@@ -10,6 +10,7 @@ const run = async (cmd) => {
   const { code, stdout, stderr } = await command.output();
 
   const removeAnsi = (input) => {
+    // deno-lint-ignore no-control-regex
     const ansiEscapeSequences = /\u001b\[[0-9;]*[a-zA-Z]/g;
     return input.replace(ansiEscapeSequences, '');
   }
@@ -40,8 +41,16 @@ Deno.test("Given API", async (t) => {
   });
 
   const apiProcess = apiCommand.spawn();
-  
-  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  do {
+    const outputReader = apiProcess.stdout.getReader();
+    const apiOutput = new TextDecoder().decode((await outputReader.read()).value);
+    outputReader.releaseLock();
+    if(apiOutput.indexOf("Listening on port 3000" != -1))
+      break;
+
+    await new Promise((resolve) => setTimeout(resolve, 100));
+  } while (true);
 
   const test = async (jsonrCommand) => {
     jsonrCommand = jsonrCommand.replace("jsonr", "deno run -A ../../main.js");
@@ -59,6 +68,6 @@ Deno.test("Given API", async (t) => {
   await test("jsonr requests/exception.http");
   await test("jsonr -s 401 requests/auth-401.http");
   await test("jsonr -e requests/environments/test.json requests/auth.http");
-
+  
   apiProcess.kill();
 });
